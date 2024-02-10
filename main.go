21a8d9fa8d70e27d6e1509e1e781b2f7c4f67fee
mon7792 <monish7792@gmail.com>
2024-02-10 17:58:23 +0530

    added person struct to show the different person variation

--- main.go
+++ main.go
@@ -43,11 +43,36 @@ func run() {
 		panic(err)
 	}
 
-	hogSprites, err := spriteSheet.Get("hedge-hog-mv-1.png")
+	// 1st hog
+	hogSprites1, err := spriteSheet.Get("hedge-hog-mv-1.png")
 	if err != nil {
 		panic(err)
 	}
-	hogPosition := win.Bounds().Center()
+	hogPosition1 := win.Bounds().Center()
+
+	// 2nd hog
+	hogSprites2, err := spriteSheet.Get("hedge-hog-mv-2.png")
+	if err != nil {
+		panic(err)
+	}
+	hogPosition2 := win.Bounds().Center()
+
+	// create a person list
+	var hogs []*Person
+
+	hogs = append(hogs, NewPerson(hogSprites1, hogPosition1, KeyBind{
+		Up:    pixelgl.KeyUp,
+		Down:  pixelgl.KeyDown,
+		Left:  pixelgl.KeyLeft,
+		Right: pixelgl.KeyRight,
+	}))
+
+	hogs = append(hogs, NewPerson(hogSprites2, hogPosition2, KeyBind{
+		Up:    pixelgl.KeyW,
+		Down:  pixelgl.KeyS,
+		Left:  pixelgl.KeyA,
+		Right: pixelgl.KeyD,
+	}))
 
 	// game loop
 	for !win.JustPressed(pixelgl.KeyEscape) {
@@ -55,26 +80,57 @@ func run() {
 		// make win blue
 		win.Clear(colornames.Skyblue)
 
-		// check for inputs
-		if win.Pressed(pixelgl.KeyLeft) {
-			hogPosition.X -= 2.0
+		// handle input
+		for i := range hogs {
+			hogs[i].HandleInput(win)
 		}
-		if win.Pressed(pixelgl.KeyRight) {
-			hogPosition.X += 2.0
-		}
-		if win.Pressed(pixelgl.KeyUp) {
-			hogPosition.Y += 2.0
-		}
-		if win.Pressed(pixelgl.KeyDown) {
-			hogPosition.Y -= 2.0
+		// draw the sprites
+		for i := range hogs {
+			hogs[i].Draw(win)
 		}
 
-		// draw the sprites
-		hogSprites.Draw(win, pixel.IM.Scaled(pixel.ZV, 2.0).Moved(hogPosition))
+		// update the window
 		win.Update()
 	}
 }
 
 func main() {
 	pixelgl.Run(run)
+}
+
+type KeyBind struct {
+	Up, Down, Left, Right pixelgl.Button
+}
+
+type Person struct {
+	Sprite  *pixel.Sprite
+	Pos     pixel.Vec
+	KeyBind KeyBind
+}
+
+func NewPerson(sprite *pixel.Sprite, pos pixel.Vec, keyBind KeyBind) *Person {
+	return &Person{
+		Sprite:  sprite,
+		Pos:     pos,
+		KeyBind: keyBind,
+	}
+}
+
+func (p *Person) Draw(win *pixelgl.Window) {
+	p.Sprite.Draw(win, pixel.IM.Scaled(pixel.ZV, 2.0).Moved(p.Pos))
+}
+
+func (p *Person) HandleInput(win *pixelgl.Window) {
+	if win.Pressed(p.KeyBind.Up) {
+		p.Pos.Y += 2.0
+	}
+	if win.Pressed(p.KeyBind.Down) {
+		p.Pos.Y -= 2.0
+	}
+	if win.Pressed(p.KeyBind.Left) {
+		p.Pos.X -= 2.0
+	}
+	if win.Pressed(p.KeyBind.Right) {
+		p.Pos.X += 2.0
+	}
 }
